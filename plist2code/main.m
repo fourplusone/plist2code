810e66c0ef286310821c9e559faffba1fcfea516
Matthias Bartelmeß <mba@fourplusone.de>
2011-12-12 00:47:18 +0100

    Initial Commit

---
+++ plist2code/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  plist2code
+//
+//  Created by Matthias Bartelmeß on 12.12.11.
+//  Copyright (c) 2011 fourplusone. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
